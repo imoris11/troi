2fc0216db805c6161e6f15fb195f94ad032137b1
Yehui Wen <wenyehui@yahoo.com>
2021-04-23 21:00:05 -0400

    fix(context): should rerender components properly

--- src/Charts/Bar.js
+++ src/Charts/Bar.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react'
+import React, { useContext, useEffect } from 'react'
+import {store, StateProvider} from '../store';
 import * as d3 from 'd3'
+import {
+  TextSmall,
+  Text,
+  Row,
+} from '../components/components'
+const BarChart = () => {
+  const globalState = useContext(store);
+  //const {dispatch} = globalState;
+  const {fileName, chemicals, locations} = globalState.state;
 
-class BarChart extends Component {
-  constructor(props) {
-    super(props)
-    this.myRef = React.createRef()
-  }
+  console.log(chemicals);
 
-  componentDidMount() {
-    const data = [12, 36, 55, 25, 35, 10, 40]
+  const myRef = React.createRef()
+  const data = [12, 36, 55, 25, 35, 10, 41];
+  const w = 900
+  const h = 600
 
-    const w = 900
-    const h = 600
-
+  useEffect(() => {
     const accessToRef = d3
-      .select(this.myRef.current)
-      .append('svg')
-      .attr('width', w)
-      .attr('height', h)
-      .style('padding', 10)
-      .style('border', '1px solid grey')
-      .style('margin-top', 10)
+    .select(myRef.current)
+    .append('svg')
+    .attr('width', w)
+    .attr('height', h)
+    .style('padding', 10)
+    .style('border', '1px solid grey')
+    .style('margin-top', 10)
 
     accessToRef
       .selectAll('rect')
@@ -32,10 +38,11 @@ class BarChart extends Component {
       .attr('width', 65)
       .attr('height', (d, i) => d * 10)
       .attr('fill', (d, i) => (d > 35 ? 'tomato' : 'yellow'))
-  }
+  })
 
-  render() {
-    return <div ref={this.myRef}></div>
-  }
+  return <StateProvider>
+      <div ref={myRef}></div>
+    </StateProvider>
 }
-export default BarChart
+
+export default BarChart;
